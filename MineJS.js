f093b2c9329ab24b4ea9404958fe80aa16bbca62
Trevorek97 <trevorek97@gmail.com>
2019-10-19 13:58:49 +0200

    * Add function to count time between start and stop new game. * Show empty fields after clicking one of them - beginnings. * Add start button. * Clear some code.

--- MineJS.js
+++ MineJS.js
@@ -13,7 +13,6 @@ function makeTable(tab,size, sign)
 }
 
 
-
 function setMines(tab, size)
 {
     var i,j;
@@ -60,21 +59,92 @@ function setNumbers(tab,size)
     }
 }
 
-function showTableA(tab,size)
+function showTable(tab,size)
 {
     var i,j;
-    var idmine =0;
+    var idmine = 0;
     for(i=0;i<size;i++)
     {
-        for (j = 0; j < size; j++)
+        for (j=0;j<size;j++)
         {
-            //document.write(tab[i][j] + ' ');
-
-           // document.write("<div class='minefield' id='1mine" + idmine + "'>" + tab[i][j] + "</div>");
-            document.write("<div class='minefield' id='idmine'>" + tab[i][j] + "</div>");
+            document.write("<div class = 'minefield' tabindex = '" + idmine + "' id = 'idmine'>" + tab[i][j] + "</div>");
             idmine++;
         }
-//        document.write("<br/>");
     }
 }
 
+
+function clickField()
+{
+    var startTime, stopTime;
+    var game=false;
+    var start=document.getElementById("startButton");
+    start.onclick=function()
+    {
+      game=true;
+      alert("Zaczynamy!");
+      startTime = timer();
+      start.onclick=function(){};
+    };
+    var colors=["black", "darkgray", "blue", "green", "red", "darkviolet", "darkred"];
+    //game=true;
+    var a = document.querySelectorAll("#idmine");
+    for(i=0;i<100;i++)
+    {
+        a[i].onclick=function()
+        {
+            if(game===true)
+            {
+                this.style.backgroundColor = colors[1];
+                if (this.innerHTML === '*')
+                {
+                    stopTime=timer();
+                    var time = stopTime-startTime;
+                    alert("GAME OVER. Czas gry: " + time + " sekund.");
+                    game = false;
+                    this.style.color = colors[0];
+                    this.style.backgroundColor = colors[4];
+                }
+                else if (this.innerHTML === '0')
+                {
+                    this.style.color = colors[1];
+                    var x = parseInt(this.getAttribute("tabindex"));
+                    var tmp = x;
+
+                    do
+                    {
+                        a[x].style.backgroundColor=colors[1];
+                        a[x].style.color=colors[1];
+                        x++;
+
+                    }while(a[x].innerHTML === '0');
+
+                    x=tmp;
+
+                    do{
+                      a[x].style.backgroundColor=colors[1];
+                      a[x].style.color=colors[1];
+                      x--;
+                    }while(a[x].innerHTML === '0');
+                }
+
+                else if (this.innerHTML === '1') this.style.color = colors[2];
+                else if (this.innerHTML === '2') this.style.color = colors[3];
+                else if (this.innerHTML === '3') this.style.color = colors[4];
+                else if (this.innerHTML === '4') this.style.color = colors[5];
+                else if (this.innerHTML === '5') this.style.color = colors[6];
+                alert(this.getAttribute("tabindex"));
+            }
+        };
+    }
+}
+
+
+function timer()
+{
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    return hours*3600 + minutes*60 + seconds;
+}
